c449e8b4b6b40f05f1dd3121fd2db1265635d06e
Digytal <open.digytal@gmail.com>
2019-02-12 20:52:22 -0200

    Spring Boot JPA - IoC @Bean e @Component

--- implementacao/cfip/src/main/java/open/digytal/cfip/CfipApplication.java
+++ implementacao/cfip/src/main/java/open/digytal/cfip/CfipApplication.java
@@ -2,13 +2,22 @@ package open.digytal.cfip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import open.digytal.cfip.controle.Calculadora;
 
 @SpringBootApplication
 public class CfipApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CfipApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(CfipApplication.class, args);
+	
+		Calculadora calc =context.getBean(Calculadora.class);
+		
+		System.out.println("Teste com Spring " + calc.somar(1, 8));
 	}
+	
 
 }
 
