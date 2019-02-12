583ac72fd7cd716cd266a121fa96596a36b6faa7
Digytal <open.digytal@gmail.com>
2019-02-12 20:24:07 -0200

    Spring Boot JPA - Criando nosso projeto

---
+++ implementacao/cfip/src/main/java/open/digytal/cfip/CfipApplication.java
@@ -0,0 +1,14 @@
+package open.digytal.cfip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfipApplication.class, args);
+	}
+
+}
+
