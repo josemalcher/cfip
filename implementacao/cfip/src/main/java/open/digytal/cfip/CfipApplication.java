8f45d18fb873db152a0309e4bca15049cef2d483
Digytal <open.digytal@gmail.com>
2019-02-14 19:39:50 -0200

    primeiro mapeamento

--- implementacao/cfip/src/main/java/open/digytal/cfip/CfipApplication.java
+++ implementacao/cfip/src/main/java/open/digytal/cfip/CfipApplication.java
@@ -1,23 +1,36 @@
 package open.digytal.cfip;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import open.digytal.cfip.controle.Calculadora;
+import open.digytal.cfip.model.Conta;
 
 @SpringBootApplication
 public class CfipApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(CfipApplication.class, args);
-	
-		Calculadora calc =context.getBean(Calculadora.class);
 		
-		System.out.println("Teste com Spring " + calc.somar(1, 8));
+		Conta corrente = new Conta();
+		corrente.setCartaoCredito(false);
+		corrente.setNome("CCT BRASIL 1879-0");
+		corrente.setSaldo(125.85);
+		
+		Conta cartaoCredito = new Conta();
+		cartaoCredito.setCartaoCredito(true);
+		cartaoCredito.setNome("CCR VISA 0701");
+		cartaoCredito.setSaldo(120.0);
+		
+		
 	}
-	
+	public static void imprimirLista(List lista) {
+		for(Object item: lista) {
+			System.out.println(item);
+		}
+	}
 
 }
 
---
+++ implementacao/cfip/src/main/java/open/digytal/cfip/model/Conta.java
@@ -0,0 +1,57 @@
+package open.digytal.cfip.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_conta")
+public class Conta implements Serializable {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(length=50,nullable=false)
+	private String nome;
+	
+	@Column(length=8,precision=2,nullable=false)
+	private Double saldo;
+	
+	@Column(name="cartao_cred", length=1,nullable=false)
+	private boolean cartaoCredito;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(Double saldo) {
+		this.saldo = saldo;
+	}
+	public boolean isCartaoCredito() {
+		return cartaoCredito;
+	}
+	public void setCartaoCredito(boolean cartaoCredito) {
+		this.cartaoCredito = cartaoCredito;
+	}
+	@Override
+	public String toString() {
+		return "Conta [id=" + id + ", nome=" + nome + ", saldo=" + saldo + ", cartaoCredito=" + cartaoCredito + "]";
+	}
+	
+}
