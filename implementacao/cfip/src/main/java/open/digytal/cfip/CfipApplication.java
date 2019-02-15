fe336219b2bc70e2a81ecbe3e04ec0b9699f1a61
Digytal <open.digytal@gmail.com>
2019-02-14 22:22:45 -0200

    Consulta JPQL - Criteria - JPA Repository

--- implementacao/cfip/src/main/java/open/digytal/cfip/CfipApplication.java
+++ implementacao/cfip/src/main/java/open/digytal/cfip/CfipApplication.java
@@ -6,24 +6,45 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import open.digytal.cfip.dao.ContaDao;
 import open.digytal.cfip.model.Conta;
+import open.digytal.cfip.repository.ContaRepository;
 
 @SpringBootApplication
 public class CfipApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(CfipApplication.class, args);
-		
+		ContaDao dao = context.getBean(ContaDao.class);
+		//FIXME:Nova implementação
+		ContaRepository repository = context.getBean(ContaRepository.class);
 		Conta corrente = new Conta();
 		corrente.setCartaoCredito(false);
 		corrente.setNome("CCT BRASIL 1879-0");
 		corrente.setSaldo(125.85);
+		
+		dao.salvar(corrente);
 		
 		Conta cartaoCredito = new Conta();
 		cartaoCredito.setCartaoCredito(true);
 		cartaoCredito.setNome("CCR VISA 0701");
 		cartaoCredito.setSaldo(120.0);
 		
+		dao.salvar(cartaoCredito);
+		
+		Conta poupanca = new Conta();
+		poupanca.setCartaoCredito(false);
+		poupanca.setNome("CPC BRASIL 0701-X");
+		poupanca.setSaldo(100.0);
+		
+		//FIXME:Nova implementação
+		repository.save(poupanca);
+		
+		//List<Conta> lista = dao.listar();
+		//List<Conta> lista = dao.listarComCriteria();
+		List<Conta> lista = repository.findAll();
+		
+		imprimirLista(lista);
 		
 	}
 	public static void imprimirLista(List lista) {
---
+++ implementacao/cfip/src/main/java/open/digytal/cfip/repository/ContaRepository.java
@@ -0,0 +1,9 @@
+package open.digytal.cfip.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import open.digytal.cfip.model.Conta;
+
+public interface ContaRepository extends JpaRepository<Conta, Integer> {
+
+}
