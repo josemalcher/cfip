5a291275f8011a5d001d14a0b5241141c2d74ed8
Gleyson <gzynho@hotmail.com>
2019-03-07 12:23:15 -0300

    GERAR DEPLOY EM MODO WAR PARA TOMCAT NAO EMBEDDED

--- implementacao/cfip-api/src/main/java/open/digytal/CfipWebApiApplication.java
+++ implementacao/cfip-api/src/main/java/open/digytal/CfipWebApiApplication.java
@@ -3,12 +3,13 @@ package open.digytal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
-public class CfipWebApiApplication {
+public class CfipWebApiApplication extends SpringBootServletInitializer { // extends War File
 	//https://www.callicoder.com/spring-boot-spring-security-jwt-mysql-react-app-part-1/
 	////https://springframework.guru/spring-requestmapping-annotation/
 	//https://www.baeldung.com/spring-data-jpa-query
