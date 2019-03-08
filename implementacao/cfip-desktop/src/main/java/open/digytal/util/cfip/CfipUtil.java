40a554bea6ba25dcc3bdf9900a932b17e9c7f70b
Gleyson <gzynho@hotmail.com>
2019-03-08 09:38:11 -0300

    classes

--- implementacao/cfip-desktop/src/main/java/open/digytal/util/cfip/CfipUtil.java
+++ implementacao/cfip-desktop/src/main/java/open/digytal/util/cfip/CfipUtil.java
@@ -2,44 +2,44 @@ package open.digytal.util.cfip;
 
 import java.util.List;
 
-import open.digytal.model.Conta;
-import open.digytal.model.Lancamento;
-import open.digytal.model.Parcela;
+import open.digytal.model.EntidadeConta;
+import open.digytal.model.EntidadeLancamento;
+import open.digytal.model.EntidadeParcela;
 import open.digytal.model.TipoMovimento;
 import open.digytal.model.Total;
 
 public class CfipUtil {
-	public static Total lancamentos(List<Lancamento> lista) {
+	public static Total EntidadeLancamentos(List<EntidadeLancamento> lista) {
 		Total total = new Total();
-        for (Lancamento l : lista) {
+        for (EntidadeLancamento l : lista) {
         	total.aplicar(l.getTipoMovimento() == TipoMovimento.C, l.getParcelamento().getRestante());
         }
         return total;
     }
-	public static Total extrato(List<Lancamento> lista) {
+	public static Total extrato(List<EntidadeLancamento> lista) {
 		Total total = new Total();
-        for (Lancamento l : lista) {
+        for (EntidadeLancamento l : lista) {
         	total.aplicar(l.getTipoMovimento() == TipoMovimento.C, l.getValor());
         }
         return total;
     }
-	public static Total previsoes(List<Lancamento> lista) {
+	public static Total previsoes(List<EntidadeLancamento> lista) {
 		Total total = new Total();
-        for (Lancamento l : lista) {
+        for (EntidadeLancamento l : lista) {
         	total.aplicar(l.getTipoMovimento() == TipoMovimento.C, l.getParcelamento().getRestante());
         }
         return total;
     }
-	public static Total parcelas(List<Parcela> lista) {
+	public static Total EntidadeParcelas(List<EntidadeParcela> lista) {
 		Total total = new Total();
-        for (Parcela p : lista) {
+        for (EntidadeParcela p : lista) {
         	total.aplicar(p.getLancamento().getTipoMovimento() == TipoMovimento.C, p.getValor());
         }
         return total;
     }
-    public static Double totalContas(List<Conta> lista) {
+    public static Double totalContas(List<EntidadeConta> lista) {
         Double total = 0.0d;
-        for (Conta c : lista) {
+        for (EntidadeConta c : lista) {
             total = total + c.getSaldoAtual();
         }
         return total;
