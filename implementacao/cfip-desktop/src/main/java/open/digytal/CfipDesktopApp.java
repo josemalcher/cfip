65f49eefc17c2d23cef9563244d32a98e5c5040c
Gleyson <gzynho@hotmail.com>
2019-03-08 15:16:16 -0300

    LancamentoTesteController

--- implementacao/cfip-desktop/src/main/java/open/digytal/CfipDesktopApp.java
+++ implementacao/cfip-desktop/src/main/java/open/digytal/CfipDesktopApp.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 import javax.swing.UIManager;
 
+import org.hsqldb.util.DatabaseManagerSwing;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -26,19 +27,24 @@ public class CfipDesktopApp {
 	}
 
 	private static void init(String[] args) {
+		base();
 		//DesktopApp.exibirSplash();
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(CfipDesktopApp.class);
+		builder.headless(false);
+		contexto = builder.run(args);
+		/*LoginPanel login = CfipDesktopApp.getBean(LoginPanel.class);
+		login.exibir();
+		*/
+	}
+	private static void base() {
 		String FILE_URL = Objects.toString(System.getProperty("db.url"), "file:/opendigytal/cfip/database/cfipdb");
 		System.out.println("Iniciando o HSQLDB em " + FILE_URL);
 		final String[] dbArg = { "--database.0", FILE_URL, "--dbname.0", "cfipdb", "--port", "5454" };
 		org.hsqldb.server.Server.main(dbArg);
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(CfipDesktopApp.class);
-		builder.headless(false);
-		contexto = builder.run(args);
-		LoginPanel login = CfipDesktopApp.getBean(LoginPanel.class);
-		login.exibir();
 		
+		final String[] serveArgs = { "--user", "sa", "--password", "", "--url", "jdbc:hsqldb:hsql://localhost:5454/cfipdb"};
+		DatabaseManagerSwing.main(serveArgs);
 	}
-
 	public static <T> T getBean(Class classe) {
 		return (T) contexto.getBean(classe);
 	}
