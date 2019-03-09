eedeeda113366dc4e3a5aebb497fa089e7ff7ab6
Gleyson <gzynho@hotmail.com>
2019-03-09 13:16:14 -0300

    ajustes

--- implementacao/cfip-web-api/src/main/java/open/digytal/webapi/secutiry/service/JwtUserService.java
+++ implementacao/cfip-web-api/src/main/java/open/digytal/webapi/secutiry/service/JwtUserService.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import open.digytal.model.EntidadeUsuario;
+import open.digytal.model.acesso.EntidadeUsuario;
 import open.digytal.repository.UsuarioRepository;
 
 
