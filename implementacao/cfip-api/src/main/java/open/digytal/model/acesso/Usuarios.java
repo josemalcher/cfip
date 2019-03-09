e126b16341810ea12ad1f67a3c1508df7e58c9d5
Gleyson Sampaio <gzynho@hotmail.com>
2019-03-09 18:17:54 -0300

    criando a classe lista

---
+++ implementacao/cfip-api/src/main/java/open/digytal/model/acesso/Usuarios.java
@@ -0,0 +1,7 @@
+package open.digytal.model.acesso;
+
+public class Usuarios extends Usuario {
+	public Usuarios(String login, String nome, String email,String senha) {
+		super(login,nome,email,senha);
+	}
+}
--- implementacao/cfip-core/src/main/java/open/digytal/controller/UsuarioGController.java
+++ implementacao/cfip-core/src/main/java/open/digytal/controller/UsuarioGController.java
@@ -1,17 +1,10 @@
 package open.digytal.controller;
 
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.StringJoiner;
-
-import javax.persistence.Query;
-
 import org.springframework.stereotype.Controller;
 
 import open.digytal.model.acesso.Usuario;
 import open.digytal.repository.persistence.Controllers;
 import open.digytal.service.UsuarioService;
-import open.digytal.util.Filtro;
 
 @Controller
 public class UsuarioGController extends Controllers<Usuario> implements UsuarioService {
