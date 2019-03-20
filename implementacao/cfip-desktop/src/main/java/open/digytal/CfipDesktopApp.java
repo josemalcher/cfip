fa85762c422a51089e441131e39aa0a50f5861f9
Gleyson Sampaio <gzynho@hotmail.com>
2019-03-20 09:53:50 -0300

    criptografia no config.properties

--- implementacao/cfip-desktop/src/main/java/open/digytal/CfipDesktopApp.java
+++ implementacao/cfip-desktop/src/main/java/open/digytal/CfipDesktopApp.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 import open.digytal.util.desktop.Configuracao;
 import open.digytal.util.desktop.DesktopApp;
 import open.digytal.util.desktop.FrmConfiguracao;
 import open.digytal.util.desktop.LoginPanel;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class CfipDesktopApp {
 	private static ConfigurableApplicationContext contexto;
 	
@@ -27,10 +30,11 @@ public class CfipDesktopApp {
 	}
 
 	private static void initApp(String[] args) {
+		DesktopApp.exibirSplash();
 		if(Configuracao.iniciarConfiguracao()) {
 			FrmConfiguracao.iniciar();
 		}else {
-			DesktopApp.exibirSplash();
+			
 			SpringApplicationBuilder builder = new SpringApplicationBuilder(CfipDesktopApp.class);
 			builder.headless(false);
 			contexto = builder.run(args);
