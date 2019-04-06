2bb8d1348aba71fa5ecef21d8f728bcd62f4e4e6
Gleyson Sampaio <gzynho@hotmail.com>
2019-04-06 16:02:52 -0300

    ajustando os beans

---
+++ implementacao/cfip-client-api/src/main/java/open/digytal/bean/Beans.java
@@ -0,0 +1,14 @@
+package open.digytal.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+@Configuration
+public class Beans {
+	@Bean
+	public ObjectMapper mapper() {
+		return new ObjectMapper();
+	}
+	
+}
--- implementacao/cfip-desktop/src/main/java/open/digytal/bean/Beans.java
+++ implementacao/cfip-desktop/src/main/java/open/digytal/bean/Beans.java
@@ -1,14 +1,9 @@
 package open.digytal.bean;
 
-import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import open.digytal.model.Sessao;
 @Configuration
@@ -17,19 +12,5 @@ public class Beans {
 	@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 	public Sessao sessao() {
 		return new Sessao();
-	}
-	@Bean
-	public ObjectMapper mapper() {
-		return new ObjectMapper();
-	}
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
-	}
-	@Bean
-	public BasicTextEncryptor basicTextEncryptor() {
-		BasicTextEncryptor encryptor = new BasicTextEncryptor();
-		encryptor.setPasswordCharArray("CfipAppSecret".toCharArray());
-		return encryptor;
 	}
 }
